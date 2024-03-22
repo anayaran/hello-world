386c4bb8766832043c0fe0973b1e13ee06441551
anayaran <161417228+anayaran@users.noreply.github.com>
2024-03-22 16:42:09 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo yum update
-#sudo yum install java-11-amazon-corretto-devel -y
+sudo yum install java-17-amazon-corretto-devel -y
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.87/bin/apache-tomcat-9.0.87.tar.gz
 sudo tar -xvf apache-tomcat-9*.tar.gz
 sudo mv apache-tomcat-9.0.87 /usr/local/tomcat
